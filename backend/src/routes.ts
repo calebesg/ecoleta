5ce02021f67ec6c20b51778abd095540e85c4b20
CalebeSG <calebeguimaraes10@gmail.com>
2020-06-14 17:06:16 -0300

    Validation of input data

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -6,12 +6,19 @@ import multerConfig from './config/multer'
 import ItemController from './controller/ItemController'
 import PointController from './controller/PointController'
 
+import { pointValidator } from './validator/PointValidator'
+
 const routes = express.Router()
 const upload = multer(multerConfig)
 
 routes.get('/items', ItemController.index)
 
-routes.post('/points', upload.single('image'), PointController.create)
+routes.post(
+  '/points',
+  upload.single('image'),
+  pointValidator(),
+  PointController.create
+)
 routes.get('/points', PointController.index)
 routes.get('/points/:id', PointController.show)
 
